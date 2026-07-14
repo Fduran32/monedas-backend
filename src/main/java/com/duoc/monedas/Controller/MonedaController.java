4a19ef1a114612504a00b502667fe9cfd077ad0e
Felipe Duran <felip.duranm@duocuc.cl>
2026-07-14 17:10:12 -0400

    actualizando el controller

--- src/main/java/com/duoc/monedas/Controller/MonedaController.java
+++ src/main/java/com/duoc/monedas/Controller/MonedaController.java
@@ -1,2 +1,33 @@
 
 
+package com.duoc.monedas.Controller;
+
+import com.duoc.monedas.Model.Moneda;
+import com.duoc.monedas.Service.MonedaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MonedaController {
+
+    private final MonedaService monedaService;
+
+    public MonedaController(MonedaService monedaService) {
+        this.monedaService = monedaService;
+    }
+
+    @GetMapping("/valores")
+    public List<Moneda> getValoresReales() {
+        List<Moneda> listaMonedas = monedaService.obtenerTodasLasMonedas();
+
+        for (Moneda moneda : listaMonedas) {
+            moneda.setUltimaActualizacion(LocalDateTime.now());
+        }
+
+        return listaMonedas;
+    }
+}
