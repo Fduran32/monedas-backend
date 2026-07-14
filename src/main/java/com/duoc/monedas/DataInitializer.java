59c1be5d41fa759834a0df4b2308581a3f9d0c0d
Felipe Duran <felip.duranm@duocuc.cl>
2026-07-14 17:02:37 -0400

    mas resultados

--- src/main/java/com/duoc/monedas/DataInitializer.java
+++ src/main/java/com/duoc/monedas/DataInitializer.java
@@ -1,7 +1,5 @@
 package com.duoc.monedas;
 
-
-
 import com.duoc.monedas.Model.Moneda;
 import com.duoc.monedas.Repository.MonedaRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -19,9 +17,15 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Simulamos lo que la AWS Lambda insertará en el futuro
+        // Indicadores económicos clave para Chile
         monedaRepository.save(new Moneda("USD", "Dólar Estadounidense", 945.50, LocalDateTime.now()));
         monedaRepository.save(new Moneda("EUR", "Euro", 1020.15, LocalDateTime.now()));
+        monedaRepository.save(new Moneda("UF", "Unidad de Fomento", 37950.40, LocalDateTime.now()));
+        monedaRepository.save(new Moneda("UTM", "Unidad Tributaria Mensual", 66200.00, LocalDateTime.now()));
+        monedaRepository.save(new Moneda("ARS", "Peso Argentino", 1.05, LocalDateTime.now()));
+        monedaRepository.save(new Moneda("BRL", "Real Brasileño", 172.30, LocalDateTime.now()));
+        monedaRepository.save(new Moneda("COBRE", "Libra de Cobre (USD)", 4.15, LocalDateTime.now()));
+
         System.out.println("¡Datos iniciales de monedas insertados en la BD H2!");
     }
 }
